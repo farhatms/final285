19c3c5a29ef15fed9a3d50a0fff6c20d0dcde934
guest temp <guest120004@outlook.com>
2022-04-26 16:39:47 -0400

    changes

--- testInsertionSort.java
+++ testInsertionSort.java
@@ -9,7 +9,6 @@ public void test()
 	testPositive();
 	testNegative();
 	testMixed();
-	testDuplicates();
 }
     public testInsertionSort() {
     }
@@ -72,29 +71,4 @@ public void test()
     	assertEquals(Sortedarr[0], arr[0]);
     }
 
-    public void testDuplicates(){
-
-    	int[] arr = new int[9];
-    	arr[0] = -8;
-    	arr[1] = -9;
-    	arr[2] = 7;
-    	arr[3] = 2;
-    	arr[4] = -8;
-    	arr[5] = 2;
-    	arr[6] = -10;
-    	arr[7] = 2;
-    	arr[8] = 0;
-    	int[] Sortedarr = new int[9];
-    	Sortedarr[0] = -10;
-    	Sortedarr[1] = -9;
-    	Sortedarr[2] = -8;
-    	Sortedarr[3] = -8;
-    	Sortedarr[4] = 0;
-    	Sortedarr[5] = 2;
-    	Sortedarr[6] = 2;
-    	Sortedarr[7] = 2;
-    	Sortedarr[8] = 7;
-    	sort.basicInsertionSort(arr);
-    	assertEquals(Sortedarr[0], arr[0]);
-    }
 }
